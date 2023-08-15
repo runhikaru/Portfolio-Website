4c1853246fae2fc02e5041ba2c633672ac7763ee
Mizuno Hikaru <machikaru@usernoMacBook-Pro.local>
2023-08-16 01:32:02 +0900

    first deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:portfolio_website/layout/wrapper.dart';
+// import 'package:portfolio_website/layout/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
