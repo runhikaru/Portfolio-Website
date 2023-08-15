5491067934b6a0b695dcee97d8a7ceff67c7b5ab
Mizuno Hikaru <machikaru@usernoMacBook-Pro.local>
2023-08-16 02:59:11 +0900

    first deploy

--- lib/layout/wrapper.dart
+++ lib/layout/wrapper.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio_website/layout/M_home_page.dart';
-
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
@@ -14,8 +12,44 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     return SelectionArea(
       child: LayoutBuilder(builder: (context, constraints) {
-        return isDesktopScreen(context) ? MobileHomePage() : MobileHomePage();
+        return isDesktopScreen(context) ? Test() : Test();
       }),
+    );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  late double deviceWidth, deviceHeight;
+
+  bool isJP = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    deviceWidth = MediaQuery.of(context).size.width;
+    deviceHeight = MediaQuery.of(context).size.height;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white60,
+        elevation: 12,
+        title: const Text(
+          "Mizuno Hikaru",
+          style: TextStyle(
+              fontWeight: FontWeight.w500, fontSize: 27, color: Colors.black),
+        ),
+        // centerTitle: true,
+      ),
     );
   }
 }
