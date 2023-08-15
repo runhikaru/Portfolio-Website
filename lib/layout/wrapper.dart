01fe2939bcd77a80c56a7f65aba95f6b31f2d2f2
Mizuno Hikaru <machikaru@usernoMacBook-Pro.local>
2023-08-16 02:04:41 +0900

    first deploy

--- lib/layout/wrapper.dart
+++ lib/layout/wrapper.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_website/layout/D_home_page.dart';
 import 'package:portfolio_website/layout/M_home_page.dart';
 
+import '../Pages/Mobile/M_contact_page.dart';
+import '../utils.dart';
+import '../widget/fade_animation.dart';
+
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
@@ -14,8 +18,115 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     return SelectionArea(
       child: LayoutBuilder(builder: (context, constraints) {
-        return isDesktopScreen(context) ? MobileHomePage() : MobileHomePage();
+        return isDesktopScreen(context) ? Test() : Test();
       }),
+    );
+  }
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white60,
+        elevation: 12,
+        title: const Text(
+          "Mizuno Hikaru",
+          style: TextStyle(
+              fontWeight: FontWeight.w500, fontSize: 27, color: Colors.black),
+        ),
+        // centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: FadeAnimation(
+          delay: 0.8,
+          child: IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) {
+                      return MobileContactPage();
+                    },
+                    transitionDuration: Duration(seconds: 3),
+                    reverseTransitionDuration: Duration(seconds: 3),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      final color = ColorTween(
+                        begin: Colors.transparent,
+                        end: Colors.black, // ブラックアウト
+                        // end: Colors.white, // ホワイトアウト
+                      ).animate(CurvedAnimation(
+                        parent: animation,
+                        // 前半
+                        curve: Interval(
+                          0.0,
+                          0.5,
+                          curve: Curves.easeInOut,
+                        ),
+                      ));
+                      final opacity = Tween<double>(
+                        begin: 0.0,
+                        end: 1.0,
+                      ).animate(CurvedAnimation(
+                        parent: animation,
+                        // 後半
+                        curve: Interval(
+                          0.5,
+                          1.0,
+                          curve: Curves.easeInOut,
+                        ),
+                      ));
+                      return AnimatedBuilder(
+                        animation: animation,
+                        builder: (context, child) {
+                          return Container(
+                            color: color.value,
+                            child: Opacity(
+                              opacity: opacity.value,
+                              child: child,
+                            ),
+                          );
+                        },
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.mail,
+                size: 35,
+              )),
+        ),
+        actions: [
+          FadeAnimation(
+            delay: 0.8,
+            child: GestureDetector(
+                onTap: () {
+                  GithubURL();
+                },
+                child: Image.asset('assets/app/github_icon.png')),
+          ),
+          FadeAnimation(
+            delay: 1,
+            child: GestureDetector(
+                onTap: () {
+                  storeAppleURL();
+                },
+                child: Image.asset('assets/app/apple_store_icon.png')),
+          ),
+          FadeAnimation(
+            delay: 1.2,
+            child: GestureDetector(
+                onTap: () {
+                  storeAndroidURL();
+                },
+                child: Image.asset('assets/app/play_storeicon.png')),
+          ),
+        ],
+      ),
     );
   }
 }
